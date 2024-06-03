857cc4b566c0bcdffee67a1a0ae478dd9f78c79f
zorro199 <zaur.brayn@mail.ru>
2024-06-03 16:31:56 +0300

    Add locale storage with redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './store/store.ts'
+import { store, persistor } from './store/store.ts'
 import App from './pages/App.tsx'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+    </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/store/api.ts
+++ src/store/api.ts
@@ -1,7 +1,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { IProduct } from "../types/product.types";
+import { REHYDRATE } from 'redux-persist'
+import type { Action } from '@reduxjs/toolkit'
 
 const API_URL = "https://fakestoreapi.com/products"
+
+type RootState = any // normally inferred from state
+
+function isHydrateAction(action: Action): action is Action<typeof REHYDRATE> & {
+  key: string
+  payload: RootState
+  err: unknown
+} {
+  return action.type === REHYDRATE
+}
 
 export const api = createApi({
     reducerPath: 'api',
@@ -9,6 +21,16 @@ export const api = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: API_URL
     }),
+    extractRehydrationInfo(action, { reducerPath }): any {
+        if (isHydrateAction(action)) {
+          // when persisting the api reducer
+          if (action.key === 'key used with redux-persist') {
+            return action.payload
+          }
+          // When persisting the root reducer
+          return action.payload[api.reducerPath]
+        }
+      },
     endpoints: builder => ({
         getProduct: builder.query<IProduct[], null>({
             query: () => '',
